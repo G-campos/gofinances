e461fa7fb8dee7c78c50e2b0b2d39323028a6ea7
Gabriel de Campos <gabrielcampos@eulabs.com.br>
2022-01-21 08:52:57 -0400

    template inicial

---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+interface Props {
+    title: String;
+}
+export function Welcome({title}: Props){
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    )
+}
